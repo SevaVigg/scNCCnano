d4f3cb3dd9c804a53824c28746bc19c9fb3aaa43
Vsevolod Makeev <vsevolod.makeev@vigg.ru>
2021-05-15 12:48:37 +0300

    final cleaning

--- R/isTargetCell.r
+++ R/isTargetCell.r
@@ -26,6 +26,7 @@ if( seuratObj@project.name == "taqman"){
 	XgeneList 	<- c("pax7a", "pax7b")
 	ltHMPgeneList 	<- c( "ltk", "foxo1a")
 	if( targetCellType %in% c("M", "I", "Tl")) {return( grepl( targetCellType, cellName))}else{ 	
+	if( targetCellType %in% c("eHMP")) {return( grepl( "Tl", cellName))}else{ 	
 	if( targetCellType == "X") { sum( seuratObj@data[ XgeneList, cellName]) > 4*length( XgeneList)} else{ 
-	if( targetCellType == "ltHMP") { sum( seuratObj@data[ ltHMPgeneList, cellName]) > 3*length( ltHMPgeneList)} else return( FALSE)}}}
+	if( targetCellType == "ltHMP") { sum( seuratObj@data[ ltHMPgeneList, cellName]) > 3*length( ltHMPgeneList)} else return( FALSE)}}}}
 }
